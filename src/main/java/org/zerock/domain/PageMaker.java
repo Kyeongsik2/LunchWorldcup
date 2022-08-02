88ce03f45b54e233513fc127a42eac986b59ca47
Kyeongsik Ham <kyeongsik2@naver.com>
2022-08-02 19:23:06 +0900

    first commit

---
+++ src/main/java/org/zerock/domain/PageMaker.java
@@ -0,0 +1,22 @@
+package org.zerock.domain;
+
+import java.util.List;
+
+public class PageMaker {
+
+	public void setCri(List<BoardVO> list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCri(Criteria cri) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void setCount(List<BoardVO> list) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
